46a731f8ee165132ac30fedf12829e2ddcfb20d3
Matt Ray <mray@zenoss.com>
2008-12-23 23:40:31 +0000

    Alvarion 2.4
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@33 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.AndreaConsadori.Alvarion/ZenPacks/AndreaConsadori/Alvarion/modeler/plugins/AlvarionWI2DeviceMap.py
+++ ZenPacks.AndreaConsadori.Alvarion/ZenPacks/AndreaConsadori/Alvarion/modeler/plugins/AlvarionWI2DeviceMap.py
@@ -12,17 +12,17 @@
 
 from Products.DataCollector.plugins.CollectorPlugin import SnmpPlugin, GetMap
 
-class AlvarionDeviceMap(SnmpPlugin):
+class AlvarionWI2DeviceMap(SnmpPlugin):
     """Map mib elements from Alvarion mib to get hw and os products.
     """
 
-    maptype = "AlvarionDeviceMap" 
+    maptype = "AlvarionWI2DeviceMap" 
 
     snmpGetMap = GetMap({ 
-      '.1.3.6.1.4.1.12394.3.2.1.13.0' : 'setHWSerialNumber',
-       '.1.3.6.1.4.1.12394.3.2.9.16.1.0' : 'manufacturer',
-       '.1.3.6.1.4.1.12394.3.2.1.2.0' : 'setHWProductKey',
-       '.1.3.6.1.4.1.12394.3.2.1.5.0': 'setOSProductKey',
+       '.1.3.6.1.4.1.12394.1.10.5.6.1.1.5.0' : 'setHWSerialNumber',
+       '.1.3.6.1.4.1.12394.1.10.5.4.2.1.1.8.6.0' : 'manufacturer',
+       '.1.3.6.1.4.1.12394.1.10.5.6.1.1.1.0  ' : 'setHWProductKey',
+       '.1.3.6.1.4.1.12394.1.10.5.6.1.1.2.0': 'setOSProductKey',
          })
 
     def process(self, device, results, log):

5300a5e05fa9b6f39d4e7d8c1a81c4c61de070f2
Matt Ray <mray@zenoss.com>
2008-12-23 23:38:40 +0000

    Alvarion 2.2
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@31 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.AndreaConsadori.Alvarion/ZenPacks/AndreaConsadori/Alvarion/modeler/plugins/AlvarionDeviceMap.py
+++ ZenPacks.AndreaConsadori.Alvarion/ZenPacks/AndreaConsadori/Alvarion/modeler/plugins/AlvarionDeviceMap.py
@@ -21,7 +21,6 @@ class AlvarionDeviceMap(SnmpPlugin):
     snmpGetMap = GetMap({ 
        '.1.3.6.1.4.1.12394.3.2.9.16.1.0' : 'manufacturer',
        '.1.3.6.1.4.1.12394.3.2.1.2.0' : 'setHWProductKey',
-     #  '.1.3.6.1.4.1.12394.3.2.1.13 ' : 'setHWSerialNumber',
        '.1.3.6.1.4.1.12394.1.1.1.2.0': 'setOSProductKey',
          })
 

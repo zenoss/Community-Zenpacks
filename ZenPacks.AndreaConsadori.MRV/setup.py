f22d17c3f59cb61ea0e52fea29ec045880fc2564
Matt Ray <mray@zenoss.com>
2008-12-29 22:47:37 +0000

    MRV 2.1
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@39 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.AndreaConsadori.MRV/setup.py
+++ ZenPacks.AndreaConsadori.MRV/setup.py
@@ -15,7 +15,7 @@
 # These variables are overwritten by Zenoss when the ZenPack is exported
 # or saved.  Do not modify them directly here.
 NAME = 'ZenPacks.AndreaConsadori.MRV'
-VERSION = '2.0'
+VERSION = '2.1'
 AUTHOR = 'Andrea Consadori'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.AndreaConsadori']

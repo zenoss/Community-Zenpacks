ac63bde51ea2849688e574cb4dc3b5ec38f57bc2
Matt Ray <mray@zenoss.com>
2009-02-06 22:49:19 +0000

    fixed pathing issue with the vmware-esx.sh by adding a zProperty for it
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@98 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.VMwareEsx/ZenPacks/community/VMwareEsx/__init__.py
+++ ZenPacks.community.VMwareEsx/ZenPacks/community/VMwareEsx/__init__.py
@@ -1,4 +1,17 @@
-
 import Globals
+import os
+from os.path import join
+from Products.ZenModel.ZenPack import ZenPackBase
 from Products.CMFCore.DirectoryView import registerDirectory
+
 registerDirectory("skins", globals())
+
+libexec = os.path.join(os.path.dirname(__file__), 'libexec')
+
+class ZenPack(ZenPackBase):
+   """ ZenPacks.community.VMwareEsx loader
+   """
+
+   packZProperties = [
+       ('zZenPackscommunityVMwareEsxlibexec', libexec, 'string'),
+       ]

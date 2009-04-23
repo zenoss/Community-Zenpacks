4e73e0f448891e31bf750b69f2f10dc29215999a
Tonino Greco <Tonino.Greco@ftse.com>
2009-04-23 09:43:56 +0000

    Fixed the percentage calculation for the perf graphs.  They were being done in the incorrect direction.  The lower the toner, the higher the graph.  This has been corrected
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@181 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.PrinterToner/setup.py
+++ ZenPacks.community.PrinterToner/setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.PrinterToner"
-VERSION = "1.40"
+VERSION = "1.40.1"
 AUTHOR = "Tonino Greco"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']

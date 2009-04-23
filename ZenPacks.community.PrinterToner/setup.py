7247ef830a3d3f41f5f67afd0f2bb8872910195c
Tonino Greco <Tonino.Greco@ftse.com>
2009-04-23 09:47:25 +0000

    Changed the graph from a line to an area graph.  Make more sense to use area on a toner - shows more of a "burn-down" feel (Thanks Matt)
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@182 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.PrinterToner/setup.py
+++ ZenPacks.community.PrinterToner/setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.PrinterToner"
-VERSION = "1.40.1"
+VERSION = "1.40.2"
 AUTHOR = "Tonino Greco"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']

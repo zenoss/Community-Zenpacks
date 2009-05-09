907692ac48c0953b2cbd04fb999f1a6f97b8c0cc
Andrew Margerison <andrew.margerison@intelliflo.com>
2009-05-09 14:11:59 +0000

    Update dependency on Perfmon Update reference to perfmon.pl in the datasources Autoadd the zProperty zSQLInstance
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@204 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.altea.SQLServer/ZenPacks/altea/SQLServer/__init__.py
+++ ZenPacks.altea.SQLServer/ZenPacks/altea/SQLServer/__init__.py
@@ -1,8 +1,17 @@
 
 import Globals
 import os.path
+from Products.CMFCore.DirectoryView import registerDirectory
 
 skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
-from Products.CMFCore.DirectoryView import registerDirectory
 if os.path.isdir(skinsDir):
     registerDirectory(skinsDir, globals())
+
+from Products.ZenModel.ZenPack import ZenPackBase
+class ZenPack(ZenPackBase):
+    """ ZenPacks.altea.SQLServer loader
+    """
+
+    packZProperties = [
+            ('zSQLInstance', 'SQLSERVER', 'string'),
+        ]

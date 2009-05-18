880a4cc00154434630daca254bb4812afcd506fd
Egor Puzanov <epuzanov@gmx.de>
2009-05-18 19:48:54 +0000

    Check if template file for relname exist, if not ignore relname.
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@209 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.deviceAdvDetail/setup.py
+++ ZenPacks.community.deviceAdvDetail/setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = 'ZenPacks.community.deviceAdvDetail'
-VERSION = '1.0'
+VERSION = '1.1'
 AUTHOR = 'Egor Puzanov'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']

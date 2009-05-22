8dbb5d528d902fd795b2fb6cce6e5a00b5f790e8
Egor Puzanov <epuzanov@gmx.de>
2009-05-22 21:17:37 +0000

    prepare for component status polling over snmp.
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@214 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.deviceAdvDetail/ZenPacks/community/deviceAdvDetail/HWStatus.py
+++ ZenPacks.community.deviceAdvDetail/ZenPacks/community/deviceAdvDetail/HWStatus.py
@@ -12,11 +12,13 @@ __doc__="""HWStatus
 
 HWStatus is an abstraction of Hardware status indication.
 
-$Id: HWStatus.py,v 1.1 2009/05/21 00:26:24 egor Exp $"""
+$Id: HWStatus.py,v 1.2 2009/05/22 21:11:24 egor Exp $"""
 
-__version__ = "$Revision: 1.1 $"[11:-2]
+__version__ = "$Revision: 1.2 $"[11:-2]
 
 DOT_GREEN    = 'green'
+DOT_PURPLE   = 'purple'
+DOT_BLUE     = 'blue'
 DOT_YELLOW   = 'yellow'
 DOT_ORANGE   = 'orange'
 DOT_RED      = 'red'
@@ -45,24 +47,29 @@ class HWStatus:
         """
         Return the Dot Color based on maximal severity
         """
-        colors = ['green', 'purple', 'blue', 'yellow', 'orange', 'red']
-	if status: return self.statusmap.get(status, (DOT_GREY, SEV_WARNING, 'other'))[0]
-	try:
-	    return colors[self.ZenEventManager.getMaxSeverity(self)]
-	except:
-	    return 'grey' 
+	if not status:
+            colors = ['green','purple','blue','yellow','orange','red','grey']
+	    status = self.cacheRRDValue('status_status', self.status)
+	    try:
+                severity = self.ZenEventManager.getMaxSeverity(self)
+                if severity == 0 and self.statusmap[status][0] == 'grey':
+	            severity = 6 
+	    except:
+	        severity = 6 
+	    return colors[severity]
+	return self.statusmap.get(status, (DOT_GREY, SEV_WARNING, 'other'))[0]
 
     def statusSeverity(self, status=None):
         """
         Return the severity based on status
 	0:'Clean', 1:'Debug', 2:'Info', 3:'Warning', 4:'Error', 5:'Critical'
         """
-	if not status: status = int(self.status)
+	if not status: status = self.cacheRRDValue('status_status', self.status)
 	return self.statusmap.get(status, (DOT_GREY, SEV_WARNING, 'other'))[1]
 
     def statusString(self, status=None):
         """
         Return the status string
         """
-	if not status: status = int(self.status)
+	if not status: status = self.cacheRRDValue('status_status', self.status)
 	return self.statusmap.get(status, (DOT_GREY, SEV_WARNING, 'other'))[2]

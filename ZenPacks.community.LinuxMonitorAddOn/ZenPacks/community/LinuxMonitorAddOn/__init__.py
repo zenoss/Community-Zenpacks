9ae55bc262604bbdd4cadde78e6398a7cb0a0bd4
Matt Ray <mray@zenoss.com>
2009-06-18 21:44:59 +0000

    adds support for LinuxMonitor 1.0
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@253 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.LinuxMonitorAddOn/ZenPacks/community/LinuxMonitorAddOn/__init__.py
+++ ZenPacks.community.LinuxMonitorAddOn/ZenPacks/community/LinuxMonitorAddOn/__init__.py
@@ -1,8 +1,41 @@
-
 import Globals
 import os.path
+import logging
+from Products.ZenModel.ZenPack import ZenPackBase
+
+log = logging.getLogger('zen.LinuxMonitorAddon')
 
 skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
 from Products.CMFCore.DirectoryView import registerDirectory
 if os.path.isdir(skinsDir):
     registerDirectory(skinsDir, globals())
+
+def findLinux(dmd):
+    return dmd.findChild('Devices/Server/SSH/Linux')
+
+class ZenPack(ZenPackBase):
+    def install(self, app):
+        """
+        Set the collector plugins for Server/SSH/Linux.
+        """
+        try:
+            linux = findLinux(app.dmd)
+        except Exception, e:
+            import traceback
+            log.debug(traceback.format_exc())
+            raise Exception('Device class Server/SSH/Linux does not exist. '
+                            'Cannot install LinuxMonitor ZenPack.')
+        ZenPackBase.install(self, app)
+
+        plugins=[]
+        for plugin in linux.zCollectorPlugins:
+            plugins.append(plugin)
+
+        plugins.append('zenoss.cmd.linux.cpuinfo')
+        plugins.append('zenoss.cmd.linux.memory')
+        plugins.append('zenoss.cmd.linux.ifconfig')
+        plugins.append('zenoss.cmd.linux.netstat_an')
+        plugins.append('zenoss.cmd.linux.netstat_rn')
+        plugins.append('zenoss.cmd.linux.process')
+
+        linux.setZenProperty( 'zCollectorPlugins', plugins )

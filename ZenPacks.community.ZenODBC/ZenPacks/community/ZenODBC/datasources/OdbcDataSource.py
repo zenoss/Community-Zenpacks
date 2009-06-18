e9e1db8b8a2b74fbc6f9ce857a612875b088e7c2
Matt Ray <mray@zenoss.com>
2009-06-18 22:23:37 +0000

    ODBC Datasource
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@256 db08cffe-75b9-45d1-af1a-46ad9ef135f6

---
+++ ZenPacks.community.ZenODBC/ZenPacks/community/ZenODBC/datasources/OdbcDataSource.py
@@ -0,0 +1,170 @@
+################################################################################
+#
+# This program is part of the ZenODBC Zenpack for Zenoss.
+# Copyright (C) 2009 Egor Puzanov.
+#
+# This program can be used under the GNU General Public License version 2
+# You can find full information here: http://www.zenoss.com/oss
+#
+################################################################################
+
+__doc__="""OdbcDataSource
+
+Defines attributes for how a datasource will be graphed
+and builds the nessesary DEF and CDEF statements for it.
+
+$Id: OdbcDataSource.py,v 1.0 2009/05/15 16:33:23 egor Exp $"""
+
+__version__ = "$Revision: 1.0 $"[11:-2]
+
+from Products.ZenModel import RRDDataSource
+from Products.ZenModel.ZenPackPersistence import ZenPackPersistence
+from AccessControl import ClassSecurityInfo, Permissions
+
+from Products.ZenEvents.ZenEventClasses import Cmd_Fail
+from Products.ZenUtils.Utils import executeStreamCommand
+
+from ZenPacks.community.ZenODBC.isql import connectionString
+
+import cgi, time
+
+odbctemplate = ("echo \"%s\" | isql -b -v -x0x7C \"%s\"")
+
+class OdbcDataSource(RRDDataSource.RRDDataSource, ZenPackPersistence):
+
+    ZENPACKID = 'ZenPacks.community.ZenODBC'
+
+    sourcetypes = ('ODBC',)
+    sourcetype = 'ODBC'
+    eventClass = Cmd_Fail
+    parser = 'ZenPacks.community.ZenODBC.isql'
+    cs = ''
+    sql = ''
+    uid = ''
+    pwd = ''
+
+    _properties = RRDDataSource.RRDDataSource._properties + (
+        {'id':'parser', 'type':'string', 'mode':'w'},
+        {'id':'cs', 'type':'string', 'mode':'w'},
+        {'id':'sql', 'type':'string', 'mode':'w'},
+        {'id':'uid', 'type':'string', 'mode':'w'},
+        {'id':'pwd', 'type':'string', 'mode':'w'},
+        )
+
+    _relations = RRDDataSource.RRDDataSource._relations + (
+        )
+    
+    # Screen action bindings (and tab definitions)
+    factory_type_information = ( 
+    { 
+        'immediate_view' : 'editODBCDataSource',
+        'actions'        :
+        ( 
+            { 'id'            : 'edit'
+            , 'name'          : 'Data Source'
+            , 'action'        : 'editODBCDataSource'
+            , 'permissions'   : ( Permissions.view, )
+            },
+        )
+    },
+    )
+
+    security = ClassSecurityInfo()
+
+
+    def getDescription(self):
+        return self.sql
+
+    def useZenCommand(self):
+        return True
+
+    def zmanage_editProperties(self, REQUEST=None):
+        'add some validation'
+        if REQUEST:
+            self.cs = REQUEST.get('cs', '')
+            self.sql = REQUEST.get('sql', '')
+            self.uid = REQUEST.get('uid', '')
+            self.pwd = REQUEST.get('pwd', '')
+        return RRDDataSource.RRDDataSource.zmanage_editProperties(
+                                                                self, REQUEST)
+
+    def getCommand(self, context):
+        cs = connectionString(self.cs,uid=self.uid,pwd=self.pwd,context=context)
+        cmd = odbctemplate % (self.sql, '" "'.join(cs.isqlArgs()))
+        cmd = RRDDataSource.RRDDataSource.getCommand(self, context, cmd)
+        return cmd
+
+    security.declareProtected('Change Device', 'manage_testDataSource')
+    def manage_testDataSource(self, testDevice, REQUEST):
+        ''' Test the datasource by executing the command and outputting the
+        non-quiet results.
+        '''
+        out = REQUEST.RESPONSE
+
+        def write(lines):
+            ''' Output (maybe partial) result text.
+            '''
+            # Looks like firefox renders progressive output more smoothly
+            # if each line is stuck into a table row.  
+            startLine = '<tr><td class="tablevalues">'
+            endLine = '</td></tr>\n'
+            if out:
+                if not isinstance(lines, list):
+                    lines = [lines]
+                for l in lines:
+                    if not isinstance(l, str):
+                        l = str(l)
+                    l = l.strip()
+                    l = cgi.escape(l)
+                    l = l.replace('\n', endLine + startLine)
+                    out.write(startLine + l + endLine)
+        
+        # Determine which device to execute against
+        device = None
+        if testDevice:
+            # Try to get specified device
+            device = self.findDevice(testDevice)
+            if not device:
+                REQUEST['message'] = 'Cannot find device matching %s' % testDevice
+                return self.callZenScreen(REQUEST)
+        elif hasattr(self, 'device'):
+            # ds defined on a device, use that device
+            device = self.device()
+        elif hasattr(self, 'getSubDevicesGen'):
+            # ds defined on a device class, use any device from the class
+            try:
+                device = self.getSubDevicesGen().next()
+            except StopIteration:
+                # No devices in this class, bail out
+                pass
+        if not device:
+            REQUEST['message'] = 'Cannot determine a device to test against.'
+            return self.callZenScreen(REQUEST)
+
+        # Get the command to run
+        command = None
+        if self.sourcetype=='ODBC':
+            command = self.getCommand(device)
+        else:
+            REQUEST['message']='Unable to test %s datasources' % self.sourcetype
+            return self.callZenScreen(REQUEST)
+        if not command:
+            REQUEST['message'] = 'Unable to create test command.'
+            return self.callZenScreen(REQUEST)
+        # Render
+        header, footer = self.commandTestOutput().split('OUTPUT_TOKEN')
+        out.write(str(header))
+        
+        write('Executing command %s against %s' %(command, device.id))
+        write('')
+        start = time.time()
+        try:
+            executeStreamCommand(command, write)
+        except:
+            import sys
+            write('exception while executing command')
+            write('type: %s  value: %s' % tuple(sys.exc_info()[:2]))
+        write('')
+        write('')
+        write('DONE in %s seconds' % long(time.time() - start))
+        out.write(str(footer))

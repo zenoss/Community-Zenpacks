4952a71a3875ac24d0709bd50e7c87d6bfccfe67
Matt Ray <mray@zenoss.com>
2009-08-31 21:27:18 +0000

    Windows SNMP Performance Monitor 1.1
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@367 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.Nova.Windows.SNMPPerfMonitor/setup.py
+++ ZenPacks.Nova.Windows.SNMPPerfMonitor/setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.Nova.Windows.SNMPPerfMonitor"
-VERSION = "1.0"
+VERSION = "1.1"
 AUTHOR = "Ryan Matte"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.Nova', 'ZenPacks.Nova.Windows']

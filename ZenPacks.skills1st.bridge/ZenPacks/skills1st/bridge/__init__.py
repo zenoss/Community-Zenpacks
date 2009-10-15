86a1a85db5ab37ee985ffc2223fa4405cf6ac0b3
Matt Ray <mray@zenoss.com>
2009-10-15 19:15:12 +0000

    Added missing skins directory registration, revved to 1.0.1 and closed http://dev.zenoss.org/trac/ticket/5675
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@408 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.skills1st.bridge/ZenPacks/skills1st/bridge/__init__.py
+++ ZenPacks.skills1st.bridge/ZenPacks/skills1st/bridge/__init__.py
@@ -1,4 +1,7 @@
-
 import Globals
+import os.path
+
+skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
 from Products.CMFCore.DirectoryView import registerDirectory
-registerDirectory("skins", globals())
+if os.path.isdir(skinsDir):
+    registerDirectory(skinsDir, globals())
--- ZenPacks.skills1st.bridge/setup.py
+++ ZenPacks.skills1st.bridge/setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.skills1st.bridge"
-VERSION = "1.0"
+VERSION = "1.0.1"
 AUTHOR = "Jane Curry"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.skills1st']

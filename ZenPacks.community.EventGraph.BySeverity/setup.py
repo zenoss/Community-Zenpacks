4de48aaf0a66ab4e30333c047cac1781a1766e0a
Matt Ray <mray@zenoss.com>
2009-12-16 23:09:23 +0000

    Event Histogram 2.0
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@471 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.EventGraph.BySeverity/setup.py
+++ ZenPacks.community.EventGraph.BySeverity/setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.EventGraph.BySeverity"
-VERSION = "1.1"
+VERSION = "2.0"
 AUTHOR = "Nathan Elliott"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.EventGraph']

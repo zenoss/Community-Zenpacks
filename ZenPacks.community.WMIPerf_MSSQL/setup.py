817ce1d4eaf80b4667c3b25e80ce07968e131582
Matt Ray <mray@zenoss.com>
2010-02-09 03:19:33 +0000

    WMI MS SQL 1.4.1
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@516 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.WMIPerf_MSSQL/setup.py
+++ ZenPacks.community.WMIPerf_MSSQL/setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.WMIPerf_MSSQL"
-VERSION = "1.4"
+VERSION = "1.4.1"
 AUTHOR = "R. Esteve"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']

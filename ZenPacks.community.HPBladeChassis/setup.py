e78dd0c0d5e92add91b5480c5b31f81c7b5997b7
Matt Ray <mray@zenoss.com>
2010-02-21 14:21:26 +0000

    HP Blade Chassis 1.23
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@541 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.HPBladeChassis/setup.py
+++ ZenPacks.community.HPBladeChassis/setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.HPBladeChassis"
-VERSION = "1.19"
+VERSION = "1.23"
 AUTHOR = "David Nicklay"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']

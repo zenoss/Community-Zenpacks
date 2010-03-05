4a1d50e9d66b3dc14631ca550ea34729bfebe6b5
David Nicklay <dnicklay@web.turner.com>
2010-03-05 16:08:47 +0000

    Added more fixes
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@549 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.HPBladeChassis/ZenPacks/community/HPBladeChassis/__init__.py
+++ ZenPacks.community.HPBladeChassis/ZenPacks/community/HPBladeChassis/__init__.py
@@ -5,6 +5,7 @@ import Globals
 import os
 
 from Products.CMFCore.DirectoryView import registerDirectory
+from Products.ZenModel.DeviceClass import manage_addDeviceClass
 
 skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
 if os.path.isdir(skinsDir):
@@ -19,4 +20,25 @@ def initialize(registrar):
     permission='Add DMD Objects',
     )
 
+
+class ZenPack(ZenPackBase):
+    """ HPBladeChassis loader
+    """
+
+    def install(self, app):
+        if not hasattr(app.zport.dmd.Devices, 'BladeChassis'):
+            manage_addDeviceClass(app.zport.dmd.Devices, 'BladeChassis')
+        dc = app.zport.dmd.Devices.getOrganizer('BladeChassis')
+        dc.description = ''
+        ZenPackBase.install(self, app)
+
+    def upgrade(self, app):
+        if not hasattr(app.zport.dmd.Devices, 'BladeChassis'):
+            manage_addDeviceClass(app.zport.dmd.Devices, 'BladeChassis')
+        dc = app.zport.dmd.Devices.getOrganizer('BladeChassis')
+        dc.description = ''
+        ZenPackBase.upgrade(self, app)
+
+    def remove(self, app, leaveObjects=False):
+        ZenPackBase.remove(self, app, leaveObjects)
 

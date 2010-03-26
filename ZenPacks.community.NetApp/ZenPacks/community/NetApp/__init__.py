4840b13dca0a062af7cc3775962eaacf6b96c386
David Nicklay <dnicklay@web.turner.com>
2010-03-26 14:34:15 +0000

    fixes for the Storage MIB and Device group fixes for the object to work properly in current zenoss
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@594 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.NetApp/ZenPacks/community/NetApp/__init__.py
+++ ZenPacks.community.NetApp/ZenPacks/community/NetApp/__init__.py
@@ -2,10 +2,47 @@ import Globals
 import os.path
 
 from Products.CMFCore.DirectoryView import registerDirectory
+from Products.ZenModel.DeviceClass import manage_addDeviceClass
 from Products.ZenModel.ZenossSecurity import ZEN_VIEW
 
 skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
 if os.path.isdir(skinsDir):
     registerDirectory(skinsDir, globals())
 
+from Products.ZenModel.ZenPack import ZenPackBase
+
+class ZenPack(ZenPackBase):
+    """ NetApp Storage loader
+    """
+
+    def install(self, app):
+	# Device - Storage organizer
+        if not hasattr(app.zport.dmd.Devices, 'Storage'):
+            manage_addDeviceClass(app.zport.dmd.Devices, 'Storage')
+        dc = app.zport.dmd.Devices.getOrganizer('Storage')
+        dc.description = ''
+	# MIB - Storage organizer
+        if not hasattr(app.zport.dmd.Mibs, 'Storage'):
+            manage_addDeviceClass(app.zport.dmd.Mibs, 'Storage')
+        mg = app.zport.dmd.Mibs.getOrganizer('Storage')
+	mg.description = ''
+
+        ZenPackBase.install(self, app)
+
+    def upgrade(self, app):
+	# Device - Storage organizer
+        if not hasattr(app.zport.dmd.Devices, 'Storage'):
+            manage_addDeviceClass(app.zport.dmd.Devices, 'Storage')
+        dc = app.zport.dmd.Devices.getOrganizer('Storage')
+        dc.description = ''
+	# MIB - Storage organizer
+        if not hasattr(app.zport.dmd.Mibs, 'Storage'):
+            manage_addDeviceClass(app.zport.dmd.Mibs, 'Storage')
+        mg = app.zport.dmd.Mibs.getOrganizer('Storage')
+	mg.description = ''
+
+        ZenPackBase.upgrade(self, app)
+
+    def remove(self, app, leaveObjects=False):
+        ZenPackBase.remove(self, app, leaveObjects)
 

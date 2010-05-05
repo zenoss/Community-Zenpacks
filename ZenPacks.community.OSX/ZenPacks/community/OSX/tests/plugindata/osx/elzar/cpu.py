7f6496d9d44b404758bbb84f410dcfa38914415d
Matt Ray <mray@zenoss.com>
2010-05-05 21:12:55 +0000

    fixed tests to work
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@669 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.OSX/ZenPacks/community/OSX/tests/plugindata/osx/elzar/cpu.py
+++ ZenPacks.community.OSX/ZenPacks/community/OSX/tests/plugindata/osx/elzar/cpu.py
@@ -1,10 +1,10 @@
 {'cpu':[
-        {'0': dict(
-                compname = 'hw',
-                socket = '0',
-                clockspeed = 2400.0,
-                cacheSizeL2 = 3072,
-                description = "Intel Core 2 Duo",
-                setProductKey = ('Intel Core 2 Duo', 'Intel'),
-                ),},]
+        dict(
+            compname = 'hw',
+            socket = '0',
+            clockspeed = 2400.0,
+            cacheSizeL2 = 3072,
+            description = "Intel Core 2 Duo",
+            setProductKey = ('Intel Core 2 Duo', 'Intel'),
+            ),]
 }
--- ZenPacks.community.OSX/ZenPacks/community/OSX/tests/plugindata/osx/ndnd/cpu.py
+++ ZenPacks.community.OSX/ZenPacks/community/OSX/tests/plugindata/osx/ndnd/cpu.py
@@ -1,9 +1,9 @@
 {"cpu":[
-        {"0": dict(
-                socket = '0',
-                clockspeed = 2100.0,
-                cacheSizeL2 = 3072,
-                description = "Intel Core 2 Duo",
-                setProductKey = ('Intel Core 2 Duo', 'Intel'),
-                ),},]
-}
+        dict(
+            socket = '0',
+            clockspeed = 2100.0,
+            cacheSizeL2 = 3072,
+            description = "Intel Core 2 Duo",
+            setProductKey = ('Intel Core 2 Duo', 'Intel'),
+            ),]
+ }

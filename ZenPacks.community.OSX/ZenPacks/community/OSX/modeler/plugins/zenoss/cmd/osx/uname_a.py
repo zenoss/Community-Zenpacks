09b1c6054cc7bcdc4af06ee1929c188e028b1e00
Matt Ray <mray@zenoss.com>
2010-05-10 22:10:32 +0000

    doc cleanup
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@681 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.OSX/ZenPacks/community/OSX/modeler/plugins/zenoss/cmd/osx/uname_a.py
+++ ZenPacks.community.OSX/ZenPacks/community/OSX/modeler/plugins/zenoss/cmd/osx/uname_a.py
@@ -1,16 +1,3 @@
-###########################################################################
-#
-# This program is part of Zenoss Core, an open source monitoring platform.
-# Copyright (C) 2010, Zenoss Inc.
-#
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 2 as published by
-# the Free Software Foundation.
-#
-# For complete information please visit: http://www.zenoss.com/oss/
-#
-###########################################################################
-
 _doc__ = """uname -a
 Determine snmpSysName and setOSProductKey from the result of the uname -a command.
 """

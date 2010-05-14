346a5a2d9aac2ced8e458466fcda09c25aa836a0
Egor Puzanov <epuzanov@gmx.de>
2010-05-14 17:24:45 +0000

    release version 2.0
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@693 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.WMIPerf_Windows/setup.py
+++ ZenPacks.community.WMIPerf_Windows/setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.WMIPerf_Windows"
-VERSION = "1.99"
+VERSION = "2.0"
 AUTHOR = "Egor Puzanov"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']

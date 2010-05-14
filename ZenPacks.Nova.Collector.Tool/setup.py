118133021a927ac55f47e15f8f0279a232cd8bbe
Ryan Matte <raw@dmon.org>
2010-05-14 15:39:45 +0000

    Fixed a bug which would freeze the script
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@690 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.Nova.Collector.Tool/setup.py
+++ ZenPacks.Nova.Collector.Tool/setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.Nova.Collector.Tool"
-VERSION = "1.1"
+VERSION = "1.2"
 AUTHOR = "Ryan Matte"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.Nova', 'ZenPacks.Nova.Collector']

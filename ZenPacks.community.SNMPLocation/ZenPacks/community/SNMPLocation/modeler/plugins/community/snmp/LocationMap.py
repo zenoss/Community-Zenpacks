34461e06f4a912985a12e1e2ac17b18b81820152
Matt Ray <mray@zenoss.com>
2010-06-09 22:08:15 +0000

    put the modeler in the right directory
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@730 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.SNMPLocation/ZenPacks/community/SNMPLocation/modeler/plugins/community/snmp/LocationMap.py
+++ ZenPacks.community.SNMPLocation/ZenPacks/community/SNMPLocation/modeler/plugins/community/snmp/LocationMap.py
@@ -1,22 +1,20 @@
-import re
-import sys
-from Products.DataCollector.plugins.CollectorPlugin import SnmpPlugin, GetMap, GetTableMap
+from Products.DataCollector.plugins.CollectorPlugin import SnmpPlugin, GetMap
 
 class LocationMap(SnmpPlugin):
-
+    
     maptype = "LocationMap"
 
     snmpGetMap = GetMap({
-             '.1.3.6.1.2.1.1.6.0' : 'setLocation',
-             })
-
+            '.1.3.6.1.2.1.1.6.0' : 'setLocation',
+            })
+    
     def process(self, device, results, log):
         """Collect SNMP location information from this device"""
         log.info('processing %s for device %s', self.name(), device.id)
         getdata, tabledata = results
         om = self.objectMap(getdata)
         om.setLocation = '/%s' % (self.prepId(om.setLocation))
-
+        
         log.info('Location: %s', om.setLocation)
         return om
 

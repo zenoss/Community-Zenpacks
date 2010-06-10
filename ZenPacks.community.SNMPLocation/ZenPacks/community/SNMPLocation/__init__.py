64105fd84dcf81182ab78c1b492ddc86fa926248
Chip Holden <cholden@zenoss.com>
2010-06-10 20:55:26 +0000

    Add getLocation so that setLocation can be used for modeling
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@733 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.SNMPLocation/ZenPacks/community/SNMPLocation/__init__.py
+++ ZenPacks.community.SNMPLocation/ZenPacks/community/SNMPLocation/__init__.py
@@ -6,3 +6,11 @@ skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
 from Products.CMFCore.DirectoryView import registerDirectory
 if os.path.isdir(skinsDir):
     registerDirectory(skinsDir, globals())
+
+from Products.ZenModel.Device import Device
+
+def Device_getLocation( self ):
+    if self.location():
+        return self.location().getOrganizerName()
+
+Device.getLocation = Device_getLocation

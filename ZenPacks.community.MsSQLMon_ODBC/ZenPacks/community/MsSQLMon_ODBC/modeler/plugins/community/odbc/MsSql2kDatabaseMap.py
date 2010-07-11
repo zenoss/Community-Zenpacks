3a1fd978e2cd3576a6c9cb02cb8fb2fb450c48bd
Egor Puzanov <epuzanov@gmx.de>
2010-07-11 21:20:54 +0000

    * Zenoss 3.0 support * correct targetPythonClass value * code cleanups
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@797 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.MsSQLMon_ODBC/ZenPacks/community/MsSQLMon_ODBC/modeler/plugins/community/odbc/MsSql2kDatabaseMap.py
+++ ZenPacks.community.MsSQLMon_ODBC/ZenPacks/community/MsSQLMon_ODBC/modeler/plugins/community/odbc/MsSql2kDatabaseMap.py
@@ -1,7 +1,7 @@
 ################################################################################
 #
 # This program is part of the MsSQLMon_ODBC Zenpack for Zenoss.
-# Copyright (C) 2009 Egor Puzanov.
+# Copyright (C) 2009, 2010 Egor Puzanov.
 #
 # This program can be used under the GNU General Public License version 2
 # You can find full information here: http://www.zenoss.com/oss
@@ -12,15 +12,14 @@ __doc__="""MsSql2kDatabaseMap.py
 
 MsSql2kDatabaseMap maps the MSSQL 2000 Databases table to Database objects
 
-$Id: MsSqlDatabaseMap.py,v 1.0 2009/11/11 11:00:23 egor Exp $"""
+$Id: MsSqlDatabaseMap.py,v 1.1 2010/07/11 18:32:27 egor Exp $"""
 
-__version__ = "$Revision: 1.0 $"[11:-2]
+__version__ = "$Revision: 1.1 $"[11:-2]
 
 from Products.ZenModel.ZenPackPersistence import ZenPackPersistence
 from ZenPacks.community.ZenODBC.OdbcPlugin import OdbcPlugin
 
 class MsSql2kDatabaseMap(OdbcPlugin):
-    
 
     ZENPACKID = 'ZenPacks.community.MsSQLMon_ODBC'
 
@@ -30,51 +29,55 @@ class MsSql2kDatabaseMap(OdbcPlugin):
     modname = "ZenPacks.community.MsSQLMon_ODBC.MsSql80Database"
     deviceProperties = \
                 OdbcPlugin.deviceProperties + ('zWinUser',
-		                               'zWinPassword',
-					       'zMsSqlConnectionString',
-					       )
+                                               'zWinPassword',
+                                               'zMsSqlConnectionString',
+                                               )
 
 
     def queries(self, device):
-        cs =  ';'.join((getattr(device, 'zMsSqlConnectionString', None),
-	                'DATABASE=master',
-                        'SERVER=%s'%str(device.manageIp),
-                        'UID=%s'%getattr(device, 'zWinUser', None),
-                        'PWD=%s'%getattr(device, 'zWinPassword', None)))
+        cs = [getattr(device, 'zMsSqlConnectionString', 'DRIVER={FreeTDS}')]
+        if not cs[0].upper().__contains__('SERVER='):
+            cs.append('SERVER=%s'%device.manageIp)
+        cs.append('DATABASE=master')
+        uid = getattr(device, 'zWinUser', None)
+        if uid: cs.append('UID=%s'%uid)
+        pwd = getattr(device, 'zWinPassword', None)
+        if pwd: cs.append('PWD=%s'%pwd)
+        cs =  ';'.join(cs)
         return {
             "dbsize": (cs,
                 """USE master;
                 sp_databases;""",
-		['dbname', 'size', 'remarks']),
+                ['DATABASE_NAME', 'DATABASE_SIZE', 'REMARKS']),
             "dbtypes80":(cs,
                 """USE master;
-                SELECT name, cmptlevel, status FROM sysdatabases;""",
-		['dbname', 'type', 'status']),
+                SELECT name as dbname, cmptlevel as type, status FROM sysdatabases;""",
+                ['dbname', 'type', 'status']),
             }
 
     def process(self, device, results, log):
         log.info('processing %s for device %s', self.name(), device.id)
         types = {1: 'SQL Server',
-	        60: 'SQL Server 6.0',
-	        65: 'SQL Server 6.5',
-	        70: 'SQL Server 7.0',
-	        80: 'SQL Server 2000',
-		}
+                60: 'SQL Server 6.0',
+                65: 'SQL Server 6.5',
+                70: 'SQL Server 7.0',
+                80: 'SQL Server 2000',
+                }
         rm = self.relMap()
-	dbsize = {}
+        dbsize = {}
 
-	for db in results.get('dbsize', []):
-	    dbsize[db['dbname']] = db['size'] * 1024
-	databases = results.get('dbtypes80', None)
-        if not databases: return	    
+        for db in results.get('dbsize', []):
+            dbsize[db['DATABASE_NAME']] = db['DATABASE_SIZE'] * 1024
+        databases = results.get('dbtypes80', None)
+        if not databases: return
         for database in databases:
-	    try:
+            try:
                 om = self.objectMap(database)
-	        if not dbsize.get(om.dbname): om.monitor = False
+                if not dbsize.get(om.dbname): om.monitor = False
                 om.id = self.prepId(om.dbname)
-	        om.type = types.get(getattr(om, 'type', 1), types[1])
-	        om.blockSize = 8192
-	        om.totalBlocks = dbsize.get(om.dbname, 0) / om.blockSize
+                om.type = types.get(getattr(on, 'type' , 1), types[1])
+                om.blockSize = 8192
+                om.totalBlocks = dbsize.get(om.dbname, 0) / om.blockSize
             except AttributeError:
                 continue
             rm.append(om)

50f80256306c495f593b370ee4147bdfc8dc698a
Egor Puzanov <epuzanov@gmx.de>
2010-07-12 15:26:57 +0000

    * release 2.0
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@802 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.deviceAdvDetail/setup.py
+++ ZenPacks.community.deviceAdvDetail/setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.deviceAdvDetail"
-VERSION = "1.9.73"
+VERSION = "2.0"
 AUTHOR = "Egor Puzanov"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']

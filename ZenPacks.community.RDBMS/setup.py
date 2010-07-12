779969fa7d8192a6c9a97e5af2fd1b88a90fbc60
Egor Puzanov <epuzanov@gmx.de>
2010-07-12 15:27:51 +0000

    * release 1.2
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@803 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.RDBMS/setup.py
+++ ZenPacks.community.RDBMS/setup.py
@@ -3,12 +3,12 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.RDBMS"
-VERSION = "1.1.70"
+VERSION = "1.2"
 AUTHOR = "Egor Puzanov"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
 PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.RDBMS']
-INSTALL_REQUIRES = ['ZenPacks.community.deviceAdvDetail>=1.3']
+INSTALL_REQUIRES = ['ZenPacks.community.deviceAdvDetail>=2.0']
 COMPAT_ZENOSS_VERS = ">=2.5"
 PREV_ZENPACK_NAME = ""
 # STOP_REPLACEMENTS

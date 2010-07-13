37626e4c0112281b5a49d85b3c1bce0d7bb126c3
Egor Puzanov <epuzanov@gmx.de>
2010-07-13 17:25:25 +0000

    * release 2.0
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@815 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.HPMon/setup.py
+++ ZenPacks.community.HPMon/setup.py
@@ -3,13 +3,13 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.HPMon"
-VERSION = "1.9.70"
+VERSION = "2.0"
 AUTHOR = "Egor Puzanov"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
 PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.HPMon']
-INSTALL_REQUIRES = ['ZenPacks.community.deviceAdvDetail>=1.2']
-COMPAT_ZENOSS_VERS = ">=2.3"
+INSTALL_REQUIRES = ['ZenPacks.community.deviceAdvDetail>=1.3']
+COMPAT_ZENOSS_VERS = ">=2.5"
 PREV_ZENPACK_NAME = ""
 # STOP_REPLACEMENTS
 ################################

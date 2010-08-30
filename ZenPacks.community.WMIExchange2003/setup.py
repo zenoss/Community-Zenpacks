1b2fe2c6e037a9081b420841918582ed490887ac
Richard Esteve <r.esteve@free.fr>
2010-08-30 15:25:43 +0000

    Bug correction Version 2.0.2
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@936 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.WMIExchange2003/setup.py
+++ ZenPacks.community.WMIExchange2003/setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.WMIPerf_Exchange2003"
-VERSION = "2.0.1"
+VERSION = "2.0.2"
 AUTHOR = "Johan Keskitalo & Andreas Falk, R.Esteve"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']

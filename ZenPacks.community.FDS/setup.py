49c87d4ff30070868a4cb6bfca852a5b2cf7558e
Matt Ray <mray@zenoss.com>
2010-08-31 14:41:11 +0000

    FDS 389 LDAP Servers 1.1
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@939 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.FDS/setup.py
+++ ZenPacks.community.FDS/setup.py
@@ -3,13 +3,13 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = 'ZenPacks.community.FDS'
-VERSION = '1.0'
+VERSION = '1.1'
 AUTHOR = 'Benny Chitambira'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
 PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.FDS']
 INSTALL_REQUIRES = []
-COMPAT_ZENOSS_VERS = '>=2.4'
+COMPAT_ZENOSS_VERS = '>=3.0'
 PREV_ZENPACK_NAME = ''
 # STOP_REPLACEMENTS
 ################################

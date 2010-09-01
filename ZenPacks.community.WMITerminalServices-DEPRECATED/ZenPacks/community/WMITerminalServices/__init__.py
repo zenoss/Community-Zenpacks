451b08298abe3b451f039e500adf003cd3263e9d
Matt Ray <mray@zenoss.com>
2010-09-01 22:06:02 +0000

    deleting deprecated ZenPacks
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@946 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.WMITerminalServices-DEPRECATED/ZenPacks/community/WMITerminalServices/__init__.py
+++
@@ -1,8 +0,0 @@
-
-import Globals
-import os.path
-
-skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
-from Products.CMFCore.DirectoryView import registerDirectory
-if os.path.isdir(skinsDir):
-    registerDirectory(skinsDir, globals())

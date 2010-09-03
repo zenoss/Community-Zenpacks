f74194e2783dd9ba73c401d6530284abc2dbc806
Richard Esteve <r.esteve@free.fr>
2010-09-03 08:03:30 +0000

    Version 2.0.2
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@953 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.WMIPerf_Exchange2003/setup.py
+++ ZenPacks.community.WMIPerf_Exchange2003/setup.py
@@ -3,13 +3,13 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.WMIPerf_Exchange2003"
-VERSION = "1.1"
-AUTHOR = "Johan Keskitalo & Andreas Falk"
+VERSION = "2.0.2"
+AUTHOR = "Johan Keskitalo & Andreas Falk, R.Esteve"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
 PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.WMIPerf_Exchange2003']
-INSTALL_REQUIRES = ['ZenPacks.community.WMIDataSource>=1.0']
-COMPAT_ZENOSS_VERS = ">=2.3"
+INSTALL_REQUIRES = ['ZenPacks.community.WMIDataSource>=2.7']
+COMPAT_ZENOSS_VERS = ">=3.0"
 PREV_ZENPACK_NAME = ""
 # STOP_REPLACEMENTS
 ################################

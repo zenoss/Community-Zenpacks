5e66717334b552cd9c2e207ea9dd0400719b4b25
Egor Puzanov <epuzanov@gmx.de>
2010-09-15 19:24:05 +0000

    * fix values parsing and results sorting
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@979 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.SQLDataSource/setup.py
+++ ZenPacks.community.SQLDataSource/setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.SQLDataSource"
-VERSION = "1.2"
+VERSION = "1.3"
 AUTHOR = "Egor Puzanov"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']

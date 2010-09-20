f34ab44b1a4a067364debabb55b0a1f2abcfa1b7
Egor Puzanov <epuzanov@gmx.de>
2010-09-20 22:01:12 +0000

    * fix Database Utilization report * instance name showed as Link
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@987 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.RDBMS/ZenPacks/community/RDBMS/reports/plugins/databases.py
+++ ZenPacks.community.RDBMS/ZenPacks/community/RDBMS/reports/plugins/databases.py
@@ -12,25 +12,34 @@ __doc__="""databases
 
 Databases reports plugin
 
-$Id: databases.py,v 1.1 2010/09/12 18:28:05 egor Exp $"""
+$Id: databases.py,v 1.2 2010/09/20 23:56:15 egor Exp $"""
 
-__version__ = "$Revision: 1.1 $"[11:-2]
+__version__ = "$Revision: 1.2 $"[11:-2]
 
 import Globals
+from Products.ZenUtils.ZenTales import talesEval
 from Products.ZenReports import Utils, Utilization
 from Products.ZenReports.AliasPlugin import AliasPlugin, Column, \
                                             RRDColumnHandler, PythonColumnHandler
 
+
 class databases( AliasPlugin ):
     "The databases report"
+
+    def _getComponents(self, device, componentPath):
+        componentPath='here/%s' % componentPath
+        try:
+            return talesEval( componentPath, device )
+        except:
+            return []
 
     def getColumns(self):
         ##      alias/dp id : column name
         return [ Column( 'deviceName', PythonColumnHandler( 'device.titleOrId()' ) ),
-                 Column( 'instance', PythonColumnHandler( 'component.dbSrvInstName()' ) ),
+                 Column( 'instance', PythonColumnHandler( 'component.getDBSrvInstLink()' ) ),
                  Column( 'dbname', PythonColumnHandler( 'component.dbname' ) ),
                  Column( 'type', PythonColumnHandler( 'component.type' ) ),
-                 Column( 'usedBytes', RRDColumnHandler( 'component.usedBytes()' ) ),
+                 Column( 'usedBytes', PythonColumnHandler( 'component.usedBytes()' ) ),
                  Column( 'totalBytes', PythonColumnHandler( 'component.totalBytes()' ) ) ]
 
     def getCompositeColumns(self):

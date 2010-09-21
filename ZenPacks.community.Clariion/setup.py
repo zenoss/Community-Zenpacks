6f41ea68e6e7fe243177238bc2c11801d900b309
Richard Esteve <r.esteve@free.fr>
2010-09-21 07:17:37 +0000

    Version 1.2.1 : split of the main perf template to avoid EMC Clariion Licence errors
    git-svn-id: http://zenpacks.zenoss.org/svn/zenpacks@988 db08cffe-75b9-45d1-af1a-46ad9ef135f6

--- ZenPacks.community.Clariion/setup.py
+++ ZenPacks.community.Clariion/setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.Clariion"
-VERSION = "1.2.0"
+VERSION = "1.2.1"
 AUTHOR = "R.Esteve"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']

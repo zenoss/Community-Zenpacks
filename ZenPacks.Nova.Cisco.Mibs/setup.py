d869b692e54382edb5e99c298c77f6e6bbc32183
Kells Kearney <kkearney@zenoss.com>
2010-10-28 15:40:12 -0400

    CiscoMibs 1.1

--- ZenPacks.Nova.Cisco.Mibs/setup.py
+++ ZenPacks.Nova.Cisco.Mibs/setup.py
@@ -1,28 +1,15 @@
-###########################################################################
-#
-# This program is part of Zenoss Core, an open source monitoring platform.
-# Copyright (C) 2008, Zenoss Inc.
-#
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 2 as published by
-# the Free Software Foundation.
-#
-# For complete information please visit: http://www.zenoss.com/oss/
-#
-###########################################################################
-
 ################################
 # These variables are overwritten by Zenoss when the ZenPack is exported
 # or saved.  Do not modify them directly here.
-NAME = 'ZenPacks.Nova.Cisco.Mibs'
-VERSION = '1.0'
-AUTHOR = 'Ryan Matte'
-LICENSE = ''
+NAME = "ZenPacks.Nova.Cisco.Mibs"
+VERSION = "1.1"
+AUTHOR = "Ryan Matte"
+LICENSE = "LGPL"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.Nova', 'ZenPacks.Nova.Cisco']
 PACKAGES = ['ZenPacks', 'ZenPacks.Nova', 'ZenPacks.Nova.Cisco', 'ZenPacks.Nova.Cisco.Mibs']
 INSTALL_REQUIRES = []
-COMPAT_ZENOSS_VERS = '>=2.2'
-PREV_ZENPACK_NAME = ''
+COMPAT_ZENOSS_VERS = ">= 3.0"
+PREV_ZENPACK_NAME = "Mibs"
 # STOP_REPLACEMENTS
 ################################
 # Zenoss will not overwrite any changes you make below here.
@@ -37,35 +24,30 @@ setup(
     version = VERSION,
     author = AUTHOR,
     license = LICENSE,
-
+    
     # This is the version spec which indicates what versions of Zenoss
     # this ZenPack is compatible with
     compatZenossVers = COMPAT_ZENOSS_VERS,
-
+    
     # previousZenPackName is a facility for telling Zenoss that the name
     # of this ZenPack has changed.  If no ZenPack with the current name is
     # installed then a zenpack of this name if installed will be upgraded.
-    prevZenPackName = PREV_ZENPACK_NAME,
-
+    prevZenPackName = PREV_ZENPACK_NAME, 
+    
     # Indicate to setuptools which namespace packages the zenpack
     # participates in
     namespace_packages = NAMESPACE_PACKAGES,
-
+    
     # Tell setuptools what packages this zenpack provides.
     packages = find_packages(),
-
+    
     # Tell setuptools to figure out for itself which files to include
     # in the binary egg when it is built.
     include_package_data = True,
-
-    # Tell setuptools what non-python files should also be included
-    # with the binary egg.
-    package_data = {
-         '': ['*.txt'],
-         '':['../COPYRIGHT.txt','../LICENSE.txt'],
-         NAME: ['objects/*','skins/*/*','services/*', 'reports/*/*',
-                'modeler/*/*', 'daemons/*', 'lib/*', 'libexec/*'],
-         },
+    
+    # The MANIFEST.in file is the recommended way of including additional files
+    # in your ZenPack. package_data is another.
+    #package_data = {}
 
     # Indicate dependencies on other python modules or ZenPacks.  This line
     # is modified by zenoss when the ZenPack edit page is submitted.  Zenoss
@@ -82,5 +64,5 @@ setup(
     },
 
     # All ZenPack eggs must be installed in unzipped form.
-    zip_safe = False,
+    zip_safe = False,    
 )
